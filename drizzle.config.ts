b7c323d3b727d18bfdfa6d0e97ca461cae74ea3d
Hiroshana <hiroshanawanni@gmail.com>
2025-07-01 20:53:50 +1000

    Configure docker and postgres

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  out: "./src/drizzle",
+  schema: "./src/drizzle/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL!,
+  },
+});
