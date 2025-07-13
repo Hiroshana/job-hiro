71f38c410f46013a668e9e7cab0821c40a83508e
Hiroshana <hiroshanawanni@gmail.com>
2025-07-13 23:06:34 +1000

    Designed Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import AppSidebarClient from "./_AppSidebarClient";
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Hiro Jobs</span>
+          </SidebarHeader>
+          <SidebarContent>aaaaa</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>dddd</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">gdgdgdgdgd</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
